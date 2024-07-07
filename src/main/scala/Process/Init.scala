76bf8bfee303cd5716fd93721c340e159e13805d
Faded-Nebula <yangzw_work@outlook.com>
2024-07-07 23:08:05 +0800

    init db

--- src/main/scala/Process/Init.scala
+++ src/main/scala/Process/Init.scala
@@ -16,8 +16,7 @@ object Init {
     for{
       _ <- API.init(config.maximumClientConnection)
       _ <- initSchema(schemaName)
-      _ <- writeDB(s"CREATE TABLE IF NOT EXISTS ${schemaName}.user_name (user_name TEXT, password TEXT)", List())
-      _ <- writeDB(s"CREATE TABLE IF NOT EXISTS ${schemaName}.doctor_rec (doctor_name TEXT, patient_name TEXT)", List())
+      _ <- writeDB(s"CREATE TABLE IF NOT EXISTS ${schemaName}.task_rec (user_name TEXT, task_name TEXT)", List())
     } yield ()
 
 }
