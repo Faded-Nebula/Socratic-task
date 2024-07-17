22bef51a382f31d262e1b46d421991ffc94e5bbc
Faded-Nebula <yangzw_work@outlook.com>
2024-07-17 19:45:14 +0800

    read author debug

--- src/main/scala/Impl/ReadTaskAuthorMessagePlanner.scala
+++ src/main/scala/Impl/ReadTaskAuthorMessagePlanner.scala
@@ -13,10 +13,10 @@ import io.circe.generic.auto.*
 case class ReadTaskAuthorMessagePlanner(taskName: String, override val planContext: PlanContext) extends Planner[String]:
   override def plan(using PlanContext): IO[String] = {
     val taskListIO: IO[List[Json]] = readDBRows(
-      s"SELECT user_name FROM ${schemaName}.task_acc WHERE task_name = ?, identity = ?",
+      s"SELECT user_name FROM ${schemaName}.task_acc WHERE task_name = ? and identity = ?",
       List(
         SqlParameter("String", taskName),
-        SqlParameter("String", "author"), 
+        SqlParameter("String", "author"),
       )
     )
     taskListIO.map { taskAuthors =>
