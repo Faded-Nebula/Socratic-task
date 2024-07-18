b49b08f62df2264352d1dc39013006a7d98b9099
Faded-Nebula <yangzw_work@outlook.com>
2024-07-18 12:36:00 +0800

    add user exists check in check identity

--- src/main/scala/Impl/CheckTaskIdentityMessagePlanner.scala
+++ src/main/scala/Impl/CheckTaskIdentityMessagePlanner.scala
@@ -1,19 +1,33 @@
 package Impl
 
 import Common.API.{PlanContext, Planner}
-import Common.DBAPI.readDBString
+import Common.DBAPI.{readDBBoolean, readDBString}
 import Common.Object.SqlParameter
 import Common.ServiceUtils.schemaName
 import cats.effect.IO
 
 case class CheckTaskIdentityMessagePlanner(taskName:String, userName:String, override val planContext: PlanContext) extends Planner[String]:
   override def plan(using PlanContext): IO[String] = {
-    // Search the Task Info on the db
-    readDBString(
-      s"SELECT identity FROM ${schemaName}.task_acc WHERE task_name = ? and user_name = ?",
+    // Check if registered
+    val checkUserExists = readDBBoolean(
+      s"SELECT EXISTS(SELECT 1 FROM ${schemaName}.task_acc WHERE task_name = ? and user_name = ?)",
       List(
         SqlParameter("String", taskName),
-        SqlParameter("String", userName)
+        SqlParameter("String", userName),
       )
     )
+    // Search the Task Info on the db
+    checkUserExists.flatMap { exists =>
+      if (exists){
+        readDBString(
+          s"SELECT identity FROM ${schemaName}.task_acc WHERE task_name = ? and user_name = ?",
+          List(
+            SqlParameter("String", taskName),
+            SqlParameter("String", userName)
+          )
+        )
+      } else {
+        IO.pure(s"User $userName not registered in the task $taskName")
+      }
+    }
   }
